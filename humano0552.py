f38c9e1370ae0b33ccdb8fac9082226b9c33d0b8
Joselyn-Alejandra-Valenzuela-0412 <164534000+Joselyn-Alejandra-Valenzuela-0412@users.noreply.github.com>
2024-09-20 11:52:05 -0600

    Add files via upload
    subi archivo clase v3 humano

---
+++ humano0552.py
@@ -0,0 +1,51 @@
+print("Actividad 9 clase humano")
+print("Joselyn Valenzuela Mat:21308051280552")
+# zona de class
+class Humano0412:
+    #zona de atributos
+    edad=0
+    cabellocolor=0
+    ojoscolor=0
+    estatura=0
+    genero=0
+#zona de funciones
+    def correr0552(self,n):
+        print(f"{n} esta corriendo ufff...")
+    def hablar0552(self,n2):
+        print(f"{n2} habla mucho")
+    def dibuja0552(self,n3):
+        print(f"{n3} sabe dibujar")
+    def duerme0552(self,n4):
+        print(f"{n4} duerme mas de lo normal")
+# zona de creacion de objetos
+joselyn=Humano0412()
+johana=Humano0412()
+
+#zona de usando objetos
+print("*******************RESULTADOS PARA JOSELYN****************************")
+joselyn.edad=18
+joselyn.cabellocolor="café"
+joselyn.ojoscolor="café oscuro"
+joselyn.genero="femenino"
+print(f"edad: {joselyn.edad}")
+print(f"color de cabello: {joselyn.cabellocolor}")
+print(f"color de ojos: {joselyn.ojoscolor}")
+print(f"genero: {joselyn.genero}")
+joselyn.correr0552("joselyn")
+joselyn.hablar0552("joselyn")
+joselyn.dibuja0552("joselyn")
+joselyn.duerme0552("joselyn")
+
+print("***************RESULTADOS PARA JOHANA********************")
+johana.edad=17
+johana.cabellocolor="café oscurito"
+johana.ojoscolor="café"
+johana.genero="femenino"
+print(f"edad: {johana.edad}")
+print(f"color de cabello: {johana.cabellocolor}")
+print(f"color de ojos: {johana.ojoscolor}")
+print(f"genero: {johana.genero}")
+johana.correr0552("johana")
+johana.hablar0552("johana")
+johana.dibuja0552("johana")
+johana.duerme0552("johana")
